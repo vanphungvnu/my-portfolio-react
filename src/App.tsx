7adacb2798389b17d8413b6efaf127e46863e296
Van Tri Phung <vantriphungvnu@gmail.com>
2026-01-18 17:14:02 -0800

    feat: implement scroll to top button

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import ScrollToTopButton from "./components/ScrollToTopButton";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <Skills />
       <Projects />
       <Footer />
+      <ScrollToTopButton />
     </>
   );
 }
